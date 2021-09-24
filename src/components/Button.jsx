6757794f0e770afbfc5ea61cb848f1ccbe5bf82e
kingazawarczynska <zawarczynska.ko@gmail.com>
2021-09-24 22:17:24 +0200

    feat(button): update buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import './Button.scss'
+import React from 'react';
+import './Button.scss';
 
-function Button({name, disabled, onClick}) {
-    return (
-        <div className='button' disabled={disabled} onClick={onClick} >
-            {name}
-        </div>
-    )
+function Button({ name, disabled, onClick }) {
+	return (
+		<div className="button" disabled={disabled} onClick={onClick}>
+			{name}
+		</div>
+	);
 }
 
-export default Button
+export default Button;
